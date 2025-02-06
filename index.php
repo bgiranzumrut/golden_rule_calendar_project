c84a99005c94ea52c0afba4953f5e60ff3b0cf79
winston <andyredjohn@gmail.com>
2025-02-06 14:24:22 -0600

    public work

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-// This is the entry point of the application
-
-// Redirect to the public/index.php file
-header("Location: public/index.php");
-exit(); // Stop further execution after the redirect
-?> 
